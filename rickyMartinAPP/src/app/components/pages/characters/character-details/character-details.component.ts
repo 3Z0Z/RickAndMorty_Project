5261dd29ff868b19e1509d4b41c1acd84d5ce572
JULIAN ANDRES ARIAS CORREA <jaariasc@academia.usbbog.edu.co>
2022-03-29 21:34:00 -0500

    progress: 3/3 Project completed

--- rickyMartinAPP/src/app/components/pages/characters/character-details/character-details.component.ts
+++ rickyMartinAPP/src/app/components/pages/characters/character-details/character-details.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+import { CharacterService } from '@app/shared/services/character.service';
+import { Character } from '@app/shared/interface/character.interface';
 
 @Component({
   selector: 'app-character-details',
@@ -7,9 +15,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute,
+    private characterSvc:CharacterService,
+    private location:Location,
+    public character$: Observable<Character>) { 
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.route.params
+      .pipe(take(1))
+      .subscribe((params) => {
+        const id = params['id'];
+        this.character$ = this.characterSvc.getDetails(id);
+      })
+  }
+
+  onGoBack():void{
+    this.location.back();
+  }
 }
--- rickyMartinAPP/src/app/components/pages/characters/characters-list/characters-list.component.ts
+++ rickyMartinAPP/src/app/components/pages/characters/characters-list/characters-list.component.ts
@@ -1,10 +1,13 @@
-import { CharacterService } from './../../../../shared/services/character.service';
-import { Character } from '@app/shared/interface/character.interface'
-import { take } from 'rxjs/operators';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+
+import { take, filter } from 'rxjs/operators';
+
+import { CharacterService } from '@shared/services/character.service';
+import { Character } from '@shared/interface/character.interface'
 
 type RequestInfo={
-  next:string;
+  next:any;
 }
 
 @Component({
@@ -12,28 +15,57 @@ type RequestInfo={
   templateUrl: './characters-list.component.html',
   styleUrls: ['./characters-list.component.scss']
 })
-export class CharactersListComponent{
+export class CharactersListComponent implements OnInit{
   characters:Character[] = [];
   info:RequestInfo={
-    next:"",
+    next:null,
   };
   private pageNum = 1;
   private query: string = "";
   private hideScrollHeight = 200;
   private showScrollHeight = 500;
   
-  constructor(private characterSvc:CharacterService) { 
-
+  constructor(
+    private characterSvc:CharacterService, 
+    private route:ActivatedRoute,
+    private router:Router) { 
+      this.onUrlChange();
   }
 
-  private getDataFromService():void{
-    this.characterSvc.searchCharacters(this.query, this.pageNum)
-    .pipe(take(1))
-    .subscribe((res:any) => {
-      const {info, results} = res;
-      this.characters = [ ... this.characters, ... results];
-      this.info = info;
+  ngOnInit(): void {
+      this.getCharactersByQuery();
+  }
+
+  private onUrlChange():void{
+    this.router.events
+    .pipe(filter((event) => event instanceof NavigationEnd))
+    .subscribe(() => {
+      this.characters = [];
+      this.pageNum = 1;
+      this.getCharactersByQuery();
     });
   }
 
+  private getCharactersByQuery():void{
+    this.route.queryParams.pipe(take(1)).subscribe((params) => {
+      console.log('Params ->',params)
+      this.query = params['q'];
+      this.getDataFromService();
+    })
+  }
+
+  private getDataFromService():void{
+    this.characterSvc
+      .searchCharacters(this.query, this.pageNum)
+      .pipe(take(1))
+      .subscribe((res:any) => {
+        if(res?.results?.length){
+          const {info, results} = res;
+        this.characters = [ ...this.characters, ...results];
+        this.info = info;
+        }else{
+          this.characters = [];
+        }
+      });
+  }
 }
